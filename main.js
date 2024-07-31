5cc4acfa99b512ef84156c8566cef5dbebfb9b17
johnworker <besscomdark@gmail.com>
2024-07-31 17:25:28 +0800

    調整

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ window.addEventListener('load', function () {
             postImages: document.querySelector('.post_images').innerHTML
         };
     
-        fetch('/save', {
+        fetch('https://web-edit-psi.vercel.app/save', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
