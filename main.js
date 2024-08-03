baf4ada0f7338c3def2b89e327748158ca5a5b2c
hunte <besscomdark@gmail.com>
2024-08-03 20:35:20 +0800

    調整

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ window.addEventListener('load', function () {
             postImages: document.querySelector('.post_images').innerHTML
         };
     
-        fetch('https://johnworker.github.io/Web_Edit/save', {
+        fetch('/save', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
